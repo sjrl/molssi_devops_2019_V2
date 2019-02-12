7a3e2087da361e9456639851b7ec207d6cf41790
Sebastian Lee <sjrl423@gmail.com>
2019-02-12 13:36:44 -0500

    Added more tests to test_molssi_math

--- molssi_devops_2019/tests/test_molssi_math.py
+++ molssi_devops_2019/tests/test_molssi_math.py
@@ -8,9 +8,23 @@ import pytest
 import sys
 
 
-def test_mean():
-    num_list = [1, 2, 3, 4, 5]
-    observed = md.mean(num_list)
+@pytest.fixture
+def num_list_3():
+    return [1, 2, 3, 4, 5]
+
+
+@pytest.mark.parametrize("num_list, expected_mean", [
+    ([1, 2, 3, 4, 5], 3), 
+    ([0, 2, 4, 6], 3), 
+    ([1, 2, 3, 4], 2.5),
+    (list(range(1,100000)), 100000/2)
+    ])
+def test_many(num_list, expected_mean):
+    assert md.mean(num_list) == expected_mean
+
+
+def test_mean(num_list_3):
+    observed = md.mean(num_list_3)
     expected = 3
     assert observed == expected
 
