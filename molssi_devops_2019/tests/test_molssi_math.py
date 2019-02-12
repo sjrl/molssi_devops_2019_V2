14389b68a2a7077a134fb96c55648a1ae8a7a920
Sebastian Lee <sjrl423@gmail.com>
2019-02-12 13:43:41 -0500

    Added another example test to test_molss_math.py

--- molssi_devops_2019/tests/test_molssi_math.py
+++ molssi_devops_2019/tests/test_molssi_math.py
@@ -13,12 +13,8 @@ def num_list_3():
     return [1, 2, 3, 4, 5]
 
 
-@pytest.mark.parametrize("num_list, expected_mean", [
-    ([1, 2, 3, 4, 5], 3), 
-    ([0, 2, 4, 6], 3), 
-    ([1, 2, 3, 4], 2.5),
-    (list(range(1,100000)), 100000/2)
-    ])
+@pytest.mark.parametrize("num_list, expected_mean", [([1, 2, 3, 4, 5], 3), ([0, 2, 4, 6], 3), ([1, 2, 3, 4], 2.5),
+                                                     (list(range(1, 100000)), 100000 / 2)])
 def test_many(num_list, expected_mean):
     assert md.mean(num_list) == expected_mean
 
@@ -40,3 +36,9 @@ def test_mean_value_error():
     empty_list = []
     with pytest.raises(ValueError):
         md.mean(empty_list)
+
+
+@pytest.mark.parametrize("x", [0,1])
+@pytest.mark.parametrize("y", [2,3])
+def test_foo(x, y):
+    pass
